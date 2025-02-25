2c1ed14afd7b85803537ce42e87a8054f0e6f7b5
Ms-You <msu122005@hs.ac.kr>
2025-02-25 18:41:00 +0900

    refactor: 문장 세트 조회 시 n+1 문제 방지를 위해 fetch join 사용

--- src/main/java/com/example/integration/repository/SentenceSetRepository.java
+++ src/main/java/com/example/integration/repository/SentenceSetRepository.java
@@ -11,13 +11,14 @@ public interface SentenceSetRepository extends JpaRepository<SentenceSet, Long> 
     // List<SentenceSet> 에서 sentenceSet.getSentenceList()를 조회할 경우 n+1 문제가 발생하므로 fetch join 사용
     @Query("select ss " +
             "from SentenceSet ss " +
-//            "left join fetch ss.sentenceList " +
+            "left join fetch ss.sentenceList " +
             "where ss.isPublic = true " +
             "order by ss.createdDate desc, ss.title asc")
     List<SentenceSet> findAllWhichPublic(Pageable pageable);
 
     @Query("select ss " +
             "from SentenceSet ss " +
+            "left join fetch ss.sentenceList " +
             "where ss.title like %:keyword% " +
             "and ss.isPublic = true " +
             "order by ss.createdDate desc, ss.title asc")
@@ -25,6 +26,7 @@ public interface SentenceSetRepository extends JpaRepository<SentenceSet, Long> 
 
     @Query("select ss " +
             "from SentenceSet ss " +
+            "left join fetch ss.sentenceList " +
             "where ss.user.id = :userId " +
             "order by ss.createdDate desc, ss.title asc")
     List<SentenceSet> findAllByUserId(Long userId, Pageable pageable);
