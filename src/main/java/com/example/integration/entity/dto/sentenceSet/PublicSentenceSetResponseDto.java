2f279be403843086de5b389ba35f98b3a661a463
Ms-You <msu122005@hs.ac.kr>
2025-03-07 17:54:00 +0900

    refactor: 공용 문장 세트 응답 데이터 수정

--- src/main/java/com/example/integration/entity/dto/sentenceSet/PublicSentenceSetResponseDto.java
+++ src/main/java/com/example/integration/entity/dto/sentenceSet/PublicSentenceSetResponseDto.java
@@ -2,14 +2,20 @@ package com.example.integration.entity.dto.sentenceSet;
 
 import com.example.integration.entity.SentenceSet;
 
+import java.time.LocalDate;
+
 public record PublicSentenceSetResponseDto (
         Long id,
-        String title
+        String title,
+        int sentenceCount,
+        LocalDate createdAt
 ) {
     public PublicSentenceSetResponseDto(SentenceSet sentenceSet) {
         this(
                 sentenceSet.getId(),
-                sentenceSet.getTitle()
+                sentenceSet.getTitle(),
+                sentenceSet.getSentenceList().size(),
+                sentenceSet.getCreatedDate()
         );
     }
 
